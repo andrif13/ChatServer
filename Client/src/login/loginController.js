769934a6702c02bf71a85f236a80c774de0400a4
Andri Freysson <andrifreysson@Andris-MacBook-Pro.local>
2016-02-19 13:47:23 +0000

    Fixed login field

--- Client/src/login/loginController.js
+++ Client/src/login/loginController.js
@@ -5,19 +5,26 @@ chatApp.controller("LoginController", ["$scope", "$location", "socket", "LoggedI
 	console.log('Loggedin:', LoggedIn);
 	$scope.user = "";
 	$scope.errorMessage = "";
+	$scope.taken = ""
 
 	$scope.onLogin = function onLogin(path){
 		LoggedIn = $scope.user;
 		console.log('Loggedin: ',LoggedIn);
-		socket.emit("adduser", $scope.user, function(available, reason){
+		if($scope.user === "") {
+			$scope.errorMessage = "You have to put some name in the field";
+		}else {
+			socket.emit("adduser", $scope.user, function(available, reason){
 			console.log('adduser');
 			if (available){
 				console.log("rooms/" + $scope.user);
 				$location.path("rooms/" + $scope.user);
 			} else{
-				alert('username ' + $scope.user + ' is taken, pick another');
+				$scope.errorMessage = "This User is already taken choose another one !"
 			}
 		});
+
+		}
+		
 	};
 }]);
 

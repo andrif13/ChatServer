9c44cac81c065e9c71475098065990c157b6e77a
Sveinn Henrik Kristinsson <Svenni@Sveinns-Air.lan>
2016-02-22 15:51:35 +0000

    unban í for-loop, SVENNI

--- Client/src/room/RoomController.js
+++ Client/src/room/RoomController.js
@@ -2,36 +2,37 @@
 
 chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$location", "$timeout",
 	function ($scope, $routeParams, socket, $location, $timeout){
-		$scope.currUser = $routeParams.user;
+		var currUser = $routeParams.user;
 		$scope.roomname = $routeParams.id;
+		var roomname = $scope.roomname;
 		$scope.isAdmin = false;
 		$scope.displaySuccess = false;
 		$scope.adminsInRoom = "";
 		$scope.glued = true;
 		$scope.show = false;
 		$scope.unbanshow = false;
-		$scope.newTopic = "";
-		$scope.topic = "";
+		var newTopic = "";
 		$scope.roomPassword = "";
 		var counter = 1;
+		var count = 1;
 		$scope.isPrv = false;
-		$scope.privmessage = "";
+		var privmessage = "";
 		$scope.GetPrv = false;
-		$scope.ServMessage = "";
+		var ServMessage = "";
 		var bannedList = [];
 		$scope.showError = false;
 		$scope.doFade = false;
 
 
 		var room_obj = {
-			room: $scope.roomname,
+			room: roomname,
 		}
 
 		socket.emit('hasjoined', room_obj);
 		
 		socket.on('updatechat', function(room, messages){
 			//console.log("messages: " + messages);
-			if(room === $scope.roomname){
+			if(room === roomname){
 				$scope.messageInRoom = messages;
 			}
 		});
@@ -39,68 +40,68 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 		socket.on('updateusers', function(room, user, admins){
 			$scope.userlist = _.keys(user);
 			$scope.adminsInRoom = admins;
-			if(admins[$scope.currUser] === $scope.currUser){
+			if(admins[currUser] === currUser){
 				$scope.isAdmin = true;
-				console.log($scope.currUser, " is Admin in this room");
+				console.log(currUser, " is Admin in this room");
 			} else {
 				$scope.isAdmin = false;
-				console.log($scope.currUser, " is NOT Admin in this room");
+				console.log(currUser, " is NOT Admin in this room");
 			}
 			console.log("OPS: ", $scope.adminsInRoom);
 		});
 
 		socket.on('kicked', function(room, userKicked, kicker){
-			if($routeParams.user === userKicked && room === $scope.roomname && $routeParams.user !== kicker){
+			if($routeParams.user === userKicked && room === roomname && $routeParams.user !== kicker){
 				$scope.sucessMessage = "You have been kicked from the room!";
 				$scope.showError = false;
 				$scope.doFade = false;
 				$scope.showError = true;
 				$timeout(function(){
 					$scope.doFade = true;
-					$location.path("rooms/" + $scope.currUser);
+					$location.path("rooms/" + currUser);
 				}, 2500);
 			}
 		});
 
 		socket.on('banned', function(room, userBanned, banner){
-			if($routeParams.user === userBanned && room === $scope.roomname && $routeParams.user !== banner){
+			if($routeParams.user === userBanned && room === roomname && $routeParams.user !== banner){
 				$scope.sucessMessage = "You have been banned from the room!";
 				$scope.showError = false;
 				$scope.doFade = false;
 				$scope.showError = true;
 				$timeout(function(){
 					$scope.doFade = true;
-					$location.path("rooms/" + $scope.currUser);
+					$location.path("rooms/" + currUser);
 				}, 2500);
 			}
 		});
 		socket.on('servermessage', function(servMessage, room, servUser){
-			if(room == $scope.roomname){
+			if(room == roomname){
 				if(servMessage == "part"){
-					$scope.ServMessage = servUser + " has left " + room;
+					ServMessage = servUser + " has left " + room;
 				}
 				if(servMessage == "quit"){
-					$scope.ServMessage = servUser + " has disconnected";
+					ServMessage = servUser + " has disconnected";
 				}
 				if(servMessage == "join"){
-					$scope.ServMessage = servUser + " has joined " + room;
+					ServMessage = servUser + " has joined " + room;
 				}
 				if(servMessage == "kick"){
-					$scope.ServMessage = servUser + " has been kicked from " + room;
+					ServMessage = servUser + " has been kicked from " + room;
 				}
 				if(servMessage == "op"){
-					$scope.ServMessage = servUser + " has been made op of " + room;
+					ServMessage = servUser + " has been made op of " + room;
 				}
 				if(servMessage == "deop"){
-					$scope.ServMessage = servUser + " is no longer op " + room;
+					ServMessage = servUser + " is no longer op " + room;
 				}
 				if(servMessage == "ban"){
-					$scope.ServMessage = servUser + " has been banned from " + room;
+					ServMessage = servUser + " has been banned from " + room;
 				}
 				if(servMessage == "unban"){
-					$scope.ServMessage = servUser + " has been unbanned from " + room;
+					ServMessage = servUser + " has been unbanned from " + room;
 				}
-
+				$scope.ServMessage = ServMessage;
 			}
 		});
 		
@@ -122,7 +123,7 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 				console.log('sendMessage');	
 				var data = {
 					msg: $scope.newmessage,
-					roomName: $scope.roomname
+					roomName: roomname
 				};
 				socket.emit('sendmsg', data);
 				$scope.newmessage = "";
@@ -154,20 +155,21 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 			$scope.GetPrv = false;
 			$scope.recvUser = rUser;
 			console.log(rUser + "   Senda private");
-			if(rUser == $scope.currUser){
+			if(rUser == currUser){
 			}
 			else{
 				console.log("inní drasli");
 				$scope.isPrv = true;
 				$scope.sendPrvMessage = function(){
-					if($scope.privmessage == ""){
+					privmessage = $scope.privmessage
+					if(privmessage === ""){
 						console.log("auttt");
 					}
 					else{
 						console.log("hello");
 							var data = {
 								nick : rUser,
-								message : $scope.privmessage
+								message : privmessage
 							};
 							console.log(data);
 							socket.emit('privatemsg', data)
@@ -178,7 +180,7 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 			}
 		};
 		$scope.kickUser = function(user){
-			if(user === $scope.currUser){
+			if(user === currUser){
 			}
 			else{//console.log('User i kickUser: ', user);
 				var kickObj = {
@@ -196,12 +198,12 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 		};
 
 		$scope.banUser = function(user){
-			if(user === $scope.currUser){
+			if(user === currUser){
 			}
 			else{
 				var banObj = {
 					user: user,
-					room: $scope.roomname
+					room: roomname
 				};
 				bannedList.push(banObj);
 				console.log("Banned list: ", bannedList);
@@ -216,7 +218,7 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 		$scope.opUser = function(user){
 			var opObj = {
 				user: user,
-				room: $scope.roomname
+				room: roomname
 			};
 			socket.emit('op', opObj, function(success){
 				if(success){
@@ -226,22 +228,29 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 		};
 		$scope.unban = function(){
 			console.log("Unbannn");
-			counter++;
-			if(counter%2===0){
+			count++;
+			if(count%2===0){
 				$scope.unbanshow = true;
 			} else {
 				$scope.unbanshow = false;
 			}
-			$scope.bannedUserList = _(bannedList).filter(c => c.room = $scope.roomname).map(c => c.user).value();
-			console.log("User list yfir banned user í þessu roomi : ", $scope.bannedUserList);
+			//$scope.bannedUserList = _(bannedList).filter(c => c.room = roomname).map(c => c.user).value();
+			var bannedUserList = [];
+			for(var i = 0; i<bannedList.length; i++){
+				if(bannedList[i].room == roomname){
+					bannedUserList.push(bannedList[i].user);
+				}
+			}
+			$scope.bannedUserList = bannedUserList;
+			console.log("User list yfir banned user í þessu roomi : ", bannedUserList);
 		};
 		$scope.unbanUser = function(user){
 			var unbanObj ={
 				user: user,
-				room: $scope.roomname
+				room: roomname
 			}
 			for(var i = 0; i < bannedList.length; i++){
-				if(bannedList[i].user === user && bannedList[i].room === $scope.roomname){
+				if(bannedList[i].user === user && bannedList[i].room === roomname){
 					bannedList.splice(i,1);
 				}
 
@@ -257,29 +266,30 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 		$scope.deopUser = function(user){
 			var deopObj = {
 				user : user,
-				room : $scope.roomname
+				room : roomname
 			};
 			socket.emit('deop', deopObj)
 		};
 		$scope.leaveRoom = function(){
-			socket.emit('partroom', $scope.roomname);
-			$location.path('rooms/' + $scope.currUser);
+			socket.emit('partroom', roomname);
+			$location.path('rooms/' + currUser);
 		};
 
 		$scope.submit = function(){
-			console.log("Topic: ", $scope.newTopic);
+			console.log("Topic: ", newTopic);
 			console.log("Password: ", $scope.roomPassword);
-			if($scope.newTopic === ""){
+			newTopic = $scope.newTopic;
+			if(newTopic === ""){
 				console.log('roomtopic has to have string');
 			} else {
 				var topicObj = {
-					room: $scope.roomname,
-					topic: $scope.newTopic
+					room: roomname,
+					topic: newTopic
 				};
 				socket.emit('settopic', topicObj, function(success){
 					if(success){
 						console.log('Successfully changed topic');
-						$scope.newTopic = "";
+						newTopic = "";
 					} else {
 						console.log('Unsuccessfully changed topic');
 
@@ -290,7 +300,7 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 				console.log("password is empty");
 			} else {
 				var passObj = {
-					room: $scope.roomname,
+					room: roomname,
 					password: $scope.roomPassword
 				};
 				console.log('PASSWORD OBJECT');
@@ -308,7 +318,7 @@ chatApp.controller("RoomController", ["$scope", "$routeParams", "socket", "$loca
 
 		$scope.removePassword = function (){
 			var removePasswordObj = {
-				room: $scope.roomname
+				room: roomname
 			};
 			socket.emit('removepassword', removePasswordObj, function(success){
 				if(success){
